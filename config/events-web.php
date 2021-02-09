fef5cdd26607208762ebdad0c069e9364c864e3b
ShokirjonMK <57320165+ShokirjonMK@users.noreply.github.com>
2021-02-09 14:10:35 +0500

    Initial commit

---
+++ config/events-web.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use Yiisoft\Composer\Config\Merger\Modifier\ReverseBlockMerge;
+
+return [
+    ReverseBlockMerge::class => new ReverseBlockMerge(),
+];
---
+++ config/providers-web.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Yiisoft\Composer\Config\Merger\Modifier\ReverseBlockMerge;
+
+/** @var array $params */
+
+return [
+    ReverseBlockMerge::class => new ReverseBlockMerge(),
+];
